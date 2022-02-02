363c1b621e0d9a14312e8bf0d39957a7c51cde7c
denisbao <denis.tpd@gmail.com>
2022-02-02 08:19:00 -0300

    Completando tarefas

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -23,13 +23,17 @@ export function TaskList() {
       title: newTaskTitle,
       isComplete: false
     }
-
     setTasks(previusTasks => [...previusTasks, newTask]);
     setNewTaskTitle("");
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const editedTasks = tasks.map(task => task.id == id ? {
+      ...task, 
+      isComplete: !task.isComplete 
+    } : task);
+
+    setTasks(editedTasks);
   }
 
   function handleRemoveTask(id: number) {
